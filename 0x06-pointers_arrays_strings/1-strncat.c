9168d3d93aaffe57e29ea5dd0f5aa17641fda94c
nyambura-kiruru <nyamburakiruru@gmail.com>
2022-09-22 08:35:49 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,24 @@
+#include "main.h
+
+/**
+*strncat - concentrate two strings but add inputted number of bytes
+*@dest: string to be appended upon
+*@src: string to be compleed at end of dest
+*@n:interger parameter to compare index to
+*Returns: returns new concatenated string
+*/
+
+char *_strncat(char *dest, char *src, int n)
+{
+
+	int index = 0, dest_len = 0;
+
+	while (dest[dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index] && index < n; index++)
+		dest[dest_len++] = src[index];
+
+	return(dest);
+
+}
