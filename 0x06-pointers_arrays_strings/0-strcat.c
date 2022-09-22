3a42ffec65c83546195304693c6d796d75379438
nyambura-kiruru <nyamburakiruru@gmail.com>
2022-09-22 08:20:02 -0700

    strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,7 +9,7 @@
  * return: returns pointer to @dest
  */
 
-char*_strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src)
 {
 
 int index = 0, dest_len = 0;
@@ -17,7 +17,7 @@ int index = 0, dest_len = 0;
 	while (dest[index++])
 		dest_len++;
 
-	for(index = 0; src[index]; index++)
+	for (index = 0; src[index]; index++)
 		dest[dest_len++] = src[index];
 
 	return (dest);
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
-include <stdio.h>
+include < stdio.h >
 /**
 *void prototypes()
 *int prototypes()
@@ -9,14 +9,14 @@ include <stdio.h>
 void reverse_array(int *a, int n);
 void print_number(int n);
 void print_buffer(char *b, int size);
-int_strcmp(cgar *dest, char *src);
+int_strcmp(cgar * dest, char *src);
 char *_strcat(char *dest, char *src);
 char *_strncpy(char *dest, char *src, int n);
-char *string_toupper(char*);
+char *string_toupper(char *);
 char *cap_string(char *);
-char *leet(char*);
+char *leet(char *);
 char *rot13(char *);
-char *infinite_add(char *n1,char *n2, char *r, int size_r);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *add_strings(char *n1, char *n2, cahr *r, int r_index);
 
 #endif/*MAIN_H*/
